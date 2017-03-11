1e96286194af2fb19c7142ed2b302e326af5a76c
Viacheslav Kroilov <slavakroilov@gmail.com>
2017-03-11 12:29:25 +0100

    numdb.h initial commit
    Just a draft

--- Code/include/numdb/numdb.h
+++ Code/include/numdb/numdb.h
@@ -8,4 +8,26 @@
 #define NUMDB_NUMDB_H
 
 
+#include <type_traits>
+#include <tuple>
+
+template<typename Engine, typename UserFunc, typename... UserFuncArgs>
+class FunctionCache {
+  public:
+	typedef UserFunc user_func_t;
+	typedef std::result_of return_t;
+	typedef std::tuple<UserFuncArgs> args_tuple_t;
+
+	template<typename... EngineCtorArgs>
+	FunctionCache(EngineCtorArgs&&... engine_args) :
+			engine_(std::forward<EngineCtorArgs>(engine_args)...) {}
+
+	return_t retrieve(UserFuncArgs&&... args) {
+		return engine_.retrieve(std::forward<UserFuncArgs>(args)...);
+	}
+
+  private:
+	Engine engine_;
+};
+
 #endif //NUMDB_NUMDB_H
