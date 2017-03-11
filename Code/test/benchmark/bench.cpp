8eff0482ac6f5bf6ce18645e6533b115cdba5b4d
Viacheslav Kroilov <slavakroilov@gmail.com>
2017-03-11 15:45:48 +0100

    Benchmark initial commit

---
+++ Code/test/benchmark/bench.cpp
@@ -0,0 +1,7 @@
+/** bench.cpp
+ *  @brief
+ *
+ *  @author Viacheslav Kroilov (metopa) slavakroilov@gmail.com
+ */
+
+#include "bench.h"
---
+++ Code/test/benchmark/main.cpp
@@ -0,0 +1,16 @@
+/** main.cpp
+ *  @brief
+ *
+ *  @author Viacheslav Kroilov (metopa) slavakroilov@gmail.com
+ */
+
+#include <boost/math/distributions/normal.hpp>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using namespace boost::math;
+
+int main() {
+	return 0;
+}
