7bdf8e127d15a52eabac6f8d5499b8afc91c2202
Viacheslav Kroilov <slavakroilov@gmail.com>
2017-03-16 16:55:23 +0100

    Add parametrized splay strategy

--- Code/include/numdb/splay_tree_strategy.h
+++ Code/include/numdb/splay_tree_strategy.h
@@ -20,19 +20,39 @@ struct CanonicalSplayStrategy {
 };
 
 struct AccessCountSplayStrategy {
+	int accesses = 0;
+
 	bool shouldSplay(AccessCountSplayStrategy* child) {
 		return accesses < child->accesses;
 	}
-
 	void visited() {}
 	void accessed() {
 		accesses++;
 	}
+
 	void dumpStrategy(std::ostream& out) const {
 		out << "<accesses: " << accesses << ">";
 	}
+};
 
-	int accesses = 0;
+template <int SCORE_BOOST, int SCORE_DEGRADATION,
+		int MAX_SCORE, int INITIAL_SCORE = MAX_SCORE / 2>
+struct ParametrizedAccessCountSplayStrategy {
+	int score = INITIAL_SCORE;
+
+	bool shouldSplay(ParametrizedAccessCountSplayStrategy* child) {
+		return score < child->score;
+	}
+	void visited() {
+		score = std::max(score - SCORE_DEGRADATION, 0);
+	}
+	void accessed() {
+		score = std::min(score + SCORE_BOOST, MAX_SCORE);
+	}
+
+	void dumpStrategy(std::ostream& out) const {
+		out << "<score: " << score << '/' << MAX_SCORE << ">";
+	}
 };
 
 #endif //NUMDB_SPLAY_TREE_STRATEGY_H
