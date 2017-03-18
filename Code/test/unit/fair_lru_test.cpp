a706d0fc10e81728e6e33433f152ec369662f080
Viacheslav Kroilov <slavakroilov@gmail.com>
2017-03-18 16:57:21 +0100

    Fair LRU unit tests

---
+++ Code/test/unit/fair_lru_test.cpp
@@ -0,0 +1,68 @@
+/** @file fair_lru_test.cpp
+ *  @brief
+ *
+ *  @author Viacheslav Kroilov (metopa) <slavakroilov@gmail.com>
+ */
+ 
+
+#include <gtest/gtest.h>
+#include "numdb/fair_lru.h"
+
+TEST(FairLRU, node_insertion_extraction) {
+	using Node = FairLRU::Node;
+	FairLRU lru;
+	std::unique_ptr<Node> a (new Node());
+	EXPECT_EQ(lru.extractLruNode(), nullptr);
+	lru.insertNode(a.get());
+	EXPECT_EQ(lru.extractLruNode(), a.get());
+	EXPECT_EQ(lru.extractLruNode(), nullptr);
+	EXPECT_EQ(lru.extractLruNode(), nullptr);
+	lru.insertNode(a.get());
+	EXPECT_EQ(lru.extractLruNode(), a.get());
+	EXPECT_EQ(lru.extractLruNode(), nullptr);
+	EXPECT_EQ(lru.extractLruNode(), nullptr);
+}
+
+TEST(FairLRU, fifo_test) {
+	using Node = FairLRU::Node;
+	FairLRU lru;
+	std::unique_ptr<Node> a(new Node());
+	std::unique_ptr<Node> b(new Node());
+	std::unique_ptr<Node> c(new Node());
+
+	lru.insertNode(a.get());
+	lru.insertNode(b.get());
+	lru.insertNode(c.get());
+	EXPECT_EQ(lru.extractLruNode(), a.get());
+	EXPECT_EQ(lru.extractLruNode(), b.get());
+	EXPECT_EQ(lru.extractLruNode(), c.get());
+	EXPECT_EQ(lru.extractLruNode(), nullptr);
+}
+
+TEST(FairLRU, mark_ru_test) {
+	using Node = FairLRU::Node;
+	FairLRU lru;
+	std::unique_ptr<Node> a(new Node());
+	std::unique_ptr<Node> b(new Node());
+	std::unique_ptr<Node> c(new Node());
+
+	lru.insertNode(a.get());
+	lru.insertNode(b.get());
+	lru.insertNode(c.get());
+	lru.markRecentlyUsed(a.get());
+	lru.markRecentlyUsed(b.get());
+	EXPECT_EQ(lru.extractLruNode(), c.get());
+	EXPECT_EQ(lru.extractLruNode(), a.get());
+
+	lru.insertNode(a.get());
+	lru.markRecentlyUsed(b.get());
+	lru.insertNode(c.get());
+	lru.markRecentlyUsed(b.get());
+	lru.markRecentlyUsed(c.get());
+	EXPECT_EQ(lru.extractLruNode(), a.get());
+	EXPECT_EQ(lru.extractLruNode(), b.get());
+	EXPECT_EQ(lru.extractLruNode(), c.get());
+	EXPECT_EQ(lru.extractLruNode(), nullptr);
+}
+
+
