95c5dd31a60efc9f9b38fed0c3c0372bc531c16e
Viacheslav Kroilov <slavakroilov@gmail.com>
2017-03-18 23:26:56 +0100

    Implement LRU logic in FairLruHashTable

--- Code/include/numdb/fixed_hashtable_fair_lru.h
+++ Code/include/numdb/fixed_hashtable_fair_lru.h
@@ -11,6 +11,7 @@
 
 #include "fixed_hashtable_base.h"
 #include "utils.h"
+#include "fair_lru.h"
 
 template <typename KeyT, typename ValueT, typename HasherT>
 class FixedHashtableFairLRU;
@@ -20,7 +21,7 @@ struct HashtableTraits<FixedHashtableFairLRU<KeyT, ValueT, HasherT>> {
 	typedef KeyT key_t;
 	typedef ValueT value_t;
 	typedef HasherT hasher_t;
-	typedef Empty node_base_t;
+	typedef FairLRU::Node node_base_t;
 };
 
 template <typename KeyT, typename ValueT,
@@ -34,17 +35,29 @@ class FixedHashtableFairLRU :
 	> base_t;
 	typedef typename base_t::Node node_t;
 
+	friend base_t;
+
 	FixedHashtableFairLRU(size_t table_size, size_t max_element_count) :
 			base_t(table_size, max_element_count) {}
 
   protected:
 	void nodeAccessedImpl(node_t* node) {
+		lru_manager_.markRecentlyUsed(node);
+	}
+	void nodeInsertedImpl(node_t* node) {
+		lru_manager_.insertNode(node);
+	}
 
+	void nodeExtractedImpl(node_t* node) {
+		lru_manager_.extractNode(node);
 	}
 
 	node_t* extractLruNodeImpl() {
-		return nullptr;
+		return static_cast<node_t*>(lru_manager_.extractLruNode());
 	}
+
+  private:
+	FairLRU lru_manager_;
 };
 
 #endif //NUMDB_FIXED_HASHTABLE_FAIR_LRU_H
