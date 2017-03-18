41c3a8fc5a8df9c3e22358e2a7d15aa95f9da59e
Viacheslav Kroilov <slavakroilov@gmail.com>
2017-03-19 00:17:31 +0100

    Add more unit tests for lru hash table

--- Code/test/unit/fixed_hashtable_fair_lru_test.cpp
+++ Code/test/unit/fixed_hashtable_fair_lru_test.cpp
@@ -17,3 +17,47 @@ TEST(HashTable_FairLRU, basic) {
 	result = ht.find(30);
 	ASSERT_EQ(bool(result), false);
 }
+
+TEST(HashTable_FairLRU, tuple_key) {
+	FixedHashtableFairLRU<std::tuple<double, double, double>,
+			std::string> ht(10, 20);
+	ht.insert(std::make_tuple(0., 0., 0.), "000");
+	ht.insert(std::make_tuple(1., 0., 0.), "100");
+	ht.insert(std::make_tuple(0., 1., 0.), "010");
+	ht.insert(std::make_tuple(0., 0., 1.), "001");
+	auto result = ht.find(std::make_tuple(-0., -0., -0.));
+	ASSERT_EQ(bool(result), true);
+	ASSERT_EQ(*result, "000");
+
+	result = ht.find(std::make_tuple(1., 0., 0.));
+	ASSERT_EQ(bool(result), true);
+	ASSERT_EQ(*result, "100");
+
+	result = ht.find(std::make_tuple(0., 1, 0.));
+	ASSERT_EQ(bool(result), true);
+	ASSERT_EQ(*result, "010");
+
+	result = ht.find(std::make_tuple(0., 0., 1));
+	ASSERT_EQ(bool(result), true);
+	ASSERT_EQ(*result, "001");
+
+	result = ht.find(std::make_tuple(0., -1, 0.));
+	ASSERT_EQ(bool(result), false);
+}
+
+TEST(HashTable_FairLRU, limit_element_count_1) {
+	FixedHashtableFairLRU<int, int> ht(5, 10);
+	for (int i = 10; i < 25; i++)
+		ht.insert(i, i);
+
+	for (int i = 10; i < 15; i++) {
+		auto result = ht.find(i);
+		ASSERT_EQ(bool(result), false);
+	}
+
+	for (int i = 15; i < 25; i++) {
+		auto result = ht.find(i);
+		ASSERT_EQ(bool(result), true);
+		ASSERT_EQ(*result, i);
+	}
+}
