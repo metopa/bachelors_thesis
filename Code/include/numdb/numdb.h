18542c22993aa8f007fe77f5a7024952acf48477
Viacheslav Kroilov <slavakroilov@gmail.com>
2017-03-25 17:23:43 +0100

    Update includes in numdb.h

--- Code/include/numdb/numdb.h
+++ Code/include/numdb/numdb.h
@@ -10,5 +10,6 @@
 #include "function_cache.h"
 #include "event_counter.h"
 #include "hash_table/fixed_hashtable_fair_lru.h"
+#include "numdb/splay_tree/splay_tree_fair_lru.h"
 #include "dummy_container.h"
 #endif //NUMDB_NUMDB_H
