3db55b8b7b9a7248583a13bce9ee1f2401f1fb19
Viacheslav Kroilov <slavakroilov@gmail.com>
2017-03-25 16:30:49 +0100

    Splay tree unit test initial commit

---
+++ Code/test/unit/splay_tree_test.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+#include <numdb/splay_tree/splay_tree_fair_lru.h>
+#include <numdb/splay_tree/splay_tree_strategy.h>
+
+/** @file splay_tree.cpp
+ *  @brief
+ *
+ *  @author Viacheslav Kroilov (metopa) <slavakroilov@gmail.com>
+ */
+ 
+TEST(SplayTree, basic) {
+	using container_t = typename SplayTreeFaitLRUTypeHolder
+			<CanonicalSplayStrategy>::template containert_t<int, int>;
+	container_t tree(container_t::elementSize() * 8);
+	tree.insert(5, 5);
+	auto result = tree.find(5);
+	ASSERT_EQ(bool(result), true);
+	ASSERT_EQ(*result, 5);
+	tree.remove(5);
+	result = tree.find(5);
+	ASSERT_EQ(bool(result), false);
+}
