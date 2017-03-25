0fb3f8c109384d64d3100977eed4399de7558b03
Viacheslav Kroilov <slavakroilov@gmail.com>
2017-03-25 18:23:42 +0100

    Splay tree: add new test function

--- Code/test/unit/splay_tree_test.cpp
+++ Code/test/unit/splay_tree_test.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <numdb/splay_tree/splay_tree_fair_lru.h>
 #include <numdb/splay_tree/splay_tree_strategy.h>
+#include <murmurhash2/functor.hpp>
 
 /** @file splay_tree.cpp
  *  @brief
@@ -19,4 +20,22 @@ TEST(SplayTree, basic) {
 	tree.remove(5);
 	result = tree.find(5);
 	ASSERT_EQ(bool(result), false);
+}
+
+TEST(SplayTree, many_insertions) {
+	using container_t = typename SplayTreeFairLRUTypeHolder
+			<CanonicalSplayStrategy>::template container_t<size_t, int>;
+	container_t tree(container_t::elementSize() * 100);
+	double avg_height = 0;
+	int reps = 10000;
+	for (int i = 0; i < reps; i++) {
+		tree.insert(mmh2::getMurmurHash2(i), i);
+		tree.verifyRefToSelfIntegrity();
+		auto result = tree.find(mmh2::getMurmurHash2(i));
+		ASSERT_EQ(bool(result), true);
+		ASSERT_EQ(*result, i);
+		tree.verifyRefToSelfIntegrity();
+		avg_height += tree.computeTreeHeight();
+	}
+	//std::cout << "Average height after " << reps << " insertions and retrievals is " << avg_height / reps << std::endl;
 }
