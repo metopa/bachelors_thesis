c92e7db9dfd13d465d23a5e7990a099dcb927b5a
Viacheslav Kroilov <slavakroilov@gmail.com>
2017-03-25 19:49:40 +0100

    Splay tree: random bottom node initial commit

--- Code/include/numdb/numdb.h
+++ Code/include/numdb/numdb.h
@@ -10,6 +10,7 @@
 #include "function_cache.h"
 #include "event_counter.h"
 #include "hash_table/fixed_hashtable_fair_lru.h"
-#include "numdb/splay_tree/splay_tree_fair_lru.h"
+#include "splay_tree/splay_tree_fair_lru.h"
+#include "splay_tree/splay_tree_random_bottom_node.h"
 #include "dummy_container.h"
 #endif //NUMDB_NUMDB_H
