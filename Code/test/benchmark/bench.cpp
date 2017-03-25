41e3830e4844c327f48cbd6e6d8de7452af13ca1
Viacheslav Kroilov <slavakroilov@gmail.com>
2017-03-25 16:18:28 +0100

    Clean up bench.cpp

--- Code/test/benchmark/bench.cpp
+++ Code/test/benchmark/bench.cpp
@@ -5,9 +5,7 @@
  */
 
 #include <random>
-#include <numdb/function_cache.h>
-#include <numdb/hash_table/fixed_hashtable_fair_lru.h>
-#include <numdb/dummy_container.h>
+
 
 #include "benchmark/benchmark.h"
 
@@ -20,41 +18,8 @@ double computeScore(size_t total_retrievals,
 	return (total_retrievals - user_func_calls) / (double) total_retrievals * 100
 		   / area_under_curve;
 }
+
 //FIXME compute sigma parameter basing on the function arity
-/**
- * @arg state.range(0) argument for a Fibonacci function
- * @arg state.range(1) available memory (in KiB)
- * @arg state.range(2) desired area under curve (in percents)
- */
-void BM_Dummy(benchmark::State& state) {
-	using UserFunc = Fibonacci<int, double, double>;
-	size_t mem = static_cast<size_t>(state.range(1));
-	mem *= 1024;
-	size_t max_element_capacity = mem / UserFunc::aggregatedArgSize();
-	double area = state.range(2);
-	area /= 100;
-
-	std::random_device r;
-	std::mt19937 e(r());
-
-	double sigma = computeSigma(area, max_element_capacity);
-	std::normal_distribution<double> dist(0, sigma);
-
-	DummyFunctionCache<UserFunc, BasicEventCounter>
-			cache(UserFunc(state.range(0)), mem);
-
-	while (state.KeepRunning()) {
-		double a = dist(e);
-		double b = dist(e);
-		benchmark::DoNotOptimize(cache(a, b));
-	}
-
-	state.SetItemsProcessed(state.iterations());
-	state.counters["capacity"] = max_element_capacity;
-	state.counters["score"] = computeScore(cache.eventCounter().total_retrievals,
-										   cache.eventCounter().user_func_invocations, area);
-}
-
 /**
  * @arg state.range(0) argument for a Fibonacci function
  * @arg state.range(1) available memory (in KiB)
