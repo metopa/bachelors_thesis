613332fd179dee887c52adf9f73d258e65c24ca0
Viacheslav Kroilov <slavakroilov@gmail.com>
2017-04-15 23:25:27 +0200

    Add DEFERRED macro

--- Code/include/numdb/utils.h
+++ Code/include/numdb/utils.h
@@ -14,4 +14,18 @@ struct CacheContainerTraits {
 	static_assert(sizeof(T) < 0, "No traits specialization for this type");
 };
 
+template <class F>
+decltype(auto) defer_call(F&& f) {
+	struct foo {
+		F f;
+		~foo() { f(); };
+	};
+	return foo{std::move(f)};
+}
+
+#define TOKENPASTE_HELPER(x, y) x ## y
+#define TOKENPASTE2_HELPER(x, y) TOKENPASTE_HELPER(x, y)
+#define DEFERRED(FUNC) auto TOKENPASTE2_HELPER(_deferred_, __LINE__) = defer_call([&]() {FUNC;});
+
+
 #endif //NUMDB_BENCHMARK_UTILS_H
