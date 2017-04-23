3bea4c0ecf309ac216cbf965a3bc0e8daa13fce4
Viacheslav Kroilov <slavakroilov@gmail.com>
2017-04-23 22:41:51 +0200

    Remove redundant eviction policy implementation

--- Code/include/numdb/numdb.h
+++ Code/include/numdb/numdb.h
@@ -11,6 +11,6 @@
 #include "event_counter.h"
 #include "numdb/hash_table/fixed_hashtable_fair_lu.h"
 #include "numdb/splay_tree/splay_tree_fair_lu.h"
-#include "splay_tree/splay_tree_random_bottom_node.h"
+#include "splay_tree/splay_tree_bottom_node.h"
 #include "dummy_container.h"
 #endif //NUMDB_NUMDB_H
