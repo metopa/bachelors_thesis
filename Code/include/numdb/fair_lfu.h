45b6571effb37ac2ec213c4036d53bccdd818a1c
Viacheslav Kroilov <slavakroilov@gmail.com>
2017-04-30 00:38:59 +0200

    Move fair LFU implementation into cpp file

--- Code/include/numdb/fair_lfu.h
+++ Code/include/numdb/fair_lfu.h
@@ -24,9 +24,7 @@ namespace numdb {
 				~Node() = default;
 
 			  private:
-				bool inFreqList() const {
-					return group_next != nullptr;
-				}
+				bool inFreqList() const;
 
 				uint64_t access_frequency = 0;
 				Node* next = nullptr;
@@ -34,121 +32,25 @@ namespace numdb {
 				Node* group_next = nullptr;
 			};
 
-			FairLFU() {
-				head_.group_next = &head_;
-			}
+			FairLFU();
 			FairLFU(const FairLFU&) = delete;
 			FairLFU& operator =(const FairLFU&) = delete;
 
-			Node* extractLuNode() {
-				assert(head_.next);
-				Node* node = head_.next;
-				extractNode(node);
-				return node;
-			}
+			Node* extractLuNode();
 
-			void markRecentlyUsed(Node* node) {
-				assert(node);
-				node->access_frequency++;
-				if (node->group_next == node &&
-					(!node->next ||
-					 node->access_frequency < node->next->access_frequency))
-					return;
-				extractNode(node);
-				if (node->group_next)
-					insertAfter(node->group_next, node);
-				else {
-					Node* prev = node->prev;
-					while (!prev->group_next) {
-						assert(prev->prev);
-						prev = prev->prev;
-					}
-					insertAfter(prev, node);
-				}
+			void markRecentlyUsed(Node* node);
 
-			}
+			void insertNode(Node* node);
 
-			void insertNode(Node* node) {
-				Node* prev = head_.next ? head_.next : &head_;
-
-				node->access_frequency = prev->access_frequency + 1;
-				insertAfter(prev, node);
-			}
-
-			void extractNode(Node* node) {
-				assert(node != nullptr);
-				if (node->inFreqList()) {
-					if (node->group_next == node) {
-						if (node->prev) {
-							assert(node->prev->next == node);
-							assert(node->prev->access_frequency < node->access_frequency);
-							node->prev->next = node->next;
-						}
-						if (node->next) {
-							assert(node->next->prev == node);
-							node->next->prev = node->prev;
-						}
-					} else {
-						// promote next node in group as a group head
-						Node* new_head = node->group_next;
-						new_head->group_next = new_head->next ? new_head->next : new_head;
-						new_head->prev = node->prev;
-						new_head->next = node->next;
-						if (node->prev) {
-							assert(node->prev->next == node);
-							node->prev->next = new_head;
-						}
-						if (node->next) {
-							assert(node->next->prev == node);
-							node->next->prev = new_head;
-						}
-					}
-				} else { //Node is in a group list
-					assert(node->prev != nullptr);
-					if (node->prev->inFreqList())
-						node->prev->group_next = node->next ? node->next : node->prev;
-						// freq list node points to itself
-						// when is a single node in the group
-					else {
-						assert(node->prev->next == node);
-						node->prev->next = node->next;
-					}
-					if (node->next) {
-						assert(node->next->prev == node);
-						node->next->prev = node->prev;
-					}
-				}
-			}
+			void extractNode(Node* node);
 
 		  private:
-			void insertAfter(Node* prev, Node* node) {
-				assert(prev);
-				assert(prev->inFreqList());
-				assert(node);
-				assert(prev->access_frequency <= node->access_frequency);
-				if (prev->access_frequency == node->access_frequency) {
-					node->group_next = nullptr;
-					node->prev = prev;
-					node->next = prev->group_next == prev ? nullptr : prev->group_next;
-					prev->group_next = node;
-					if (node->next)
-						node->next->prev = node;
-				} else if (!prev->next ||
-						   prev->next->access_frequency > node->access_frequency) {
-					node->group_next = node;
-					node->next = prev->next;
-					node->prev = prev;
-					prev->next = node;
-					if (node->next) {
-						assert(node->next->prev == prev);
-						node->next->prev = node;
-					}
-				} else
-					insertAfter(prev->next, node);
-			}
+			void insertAfter(Node* prev, Node* node);
 
 			Node head_;
 		};
+
+
 	}
 }
 #endif //NUMDB_FAIR_LFU_H
