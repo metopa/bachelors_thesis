673f5ace55d84b415d93051f85034b7fbbe74c2b
Viacheslav Kroilov <slavakroilov@gmail.com>
2017-05-01 00:21:54 +0200

    Update benchmark

--- Code/test/benchmark/bench.cpp
+++ Code/test/benchmark/bench.cpp
@@ -5,14 +5,14 @@
  */
 
 #include <random>
-
-#include "benchmark/benchmark.h"
+#include <memory>
+#include <benchmark/benchmark.h>
 #include "utils.h"
 
 #include "numdb/numdb.h"
 
 #ifndef SHORT_BENCHMARK
-#define SHORT_BENCHMARK 1
+#define SHORT_BENCHMARK 0
 #endif
 
 using namespace numdb;
@@ -50,6 +50,7 @@ void BM(benchmark::State& state) {
 	FunctionCache<UserFunc, ContainerTypeHolder, utility::BasicEventCounter>
 			cache(UserFunc(state.range(0), state.range(1)), mem);
 
+
 	while (state.KeepRunning()) {
 		double a = std::round(dist(e) + mean_offset);
 		benchmark::DoNotOptimize(cache(a));
@@ -62,84 +63,146 @@ void BM(benchmark::State& state) {
 										  cache.eventCounter().user_func_invocations, area);
 }
 
-constexpr int maxKB = 2 * 1024 * 1024;
+/**
+ * @arg state.range(0) min argument for a Fibonacci function
+ * @arg state.range(1) max argument for a Fibonacci function
+ * @arg state.range(2) available memory (in KiB)
+ * @arg state.range(3) desired area under curve (in percents)
+ * @arg state.range(4) distribution slide speed * 100
+ */
+template <class ContainerTypeHolder, size_t... ContainerArgs>
+void ParallelBM(benchmark::State& state) {
+	using UserFunc = Fibonacci<int, double>;
 
-void SeqArguments(benchmark::internal::Benchmark* b) {
-	std::vector<int> areas = {30,
-	#if !SHORT_BENCHMARK
-							  75,
-	#endif
-							  95};
-	int min_memory = 100;
-	int max_memory = maxKB;
-	std::vector<std::pair<int, int>> fib_args = {
-	#if !SHORT_BENCHMARK
-	                                             {30, 30},
-												 {37, 37},
-	#endif
-												 {30, 37},
-												};
-	#if SHORT_BENCHMARK
-	constexpr int mem_multiply = 16;
-	#else
-	constexpr int mem_multiply = 8;
-	#endif
-	std::vector<int> slide = {0, 10};
+	size_t mem = static_cast<size_t>(state.range(2)) * 1024;
+	size_t max_element_capacity = mem / UserFunc::aggregatedArgSize();
+
+	double area = state.range(3);
+	area /= 100;
+
+	double mean_delta = state.range(4) / 100.;
+	double mean_offset = 0;
+
+	double sigma = computeSigma(area, max_element_capacity);
+	std::random_device r;
+	std::mt19937 e(r());
+	std::normal_distribution<double> dist(0, sigma);
+
+
+	using cache_t = FunctionCache<UserFunc, ContainerTypeHolder, utility::BasicEventCounter>;
+	static std::unique_ptr<cache_t> cache_ptr;
+
+	if (state.thread_index == 0)
+		cache_ptr.reset(new cache_t(UserFunc(state.range(0), state.range(1)), mem, ContainerArgs...));
+
+	while (state.KeepRunning()) {
+		double a = std::round(dist(e) + mean_offset);
+		benchmark::DoNotOptimize((*cache_ptr)(a));
+		mean_offset += mean_delta;
+	}
+
+	state.SetItemsProcessed(state.iterations());
+
+	if (state.thread_index == 0) {
+		state.counters["cap"] = cache_ptr->capacity();
+		state.counters["hr"] = computeHitRate(cache_ptr->eventCounter().total_retrievals,
+											  cache_ptr->eventCounter().user_func_invocations, area);
+		cache_ptr.reset();
+	}
+}
+
+constexpr int maxKB = 100 * 1024;
+constexpr int maxThreads = 4;
+
+void shortSequence(benchmark::internal::Benchmark* b) {
+	constexpr int min_memory = 1;
+	constexpr int max_memory = 256;
+	constexpr int mem_multiply = 4;
+
+	std::vector<int> areas = {85};
+	std::vector<std::pair<int, int>> fib_args = {{28, 35}};
+	std::vector<int> slide = {1};
+
+	b->ArgNames({"fib_min", "fib_max", "memory", "area", "mean_varying_rate"});
+
 	for (auto slide_speed : slide)
 		for (auto fib_arg : fib_args)
 			for (auto area : areas)
+				for (int mem = min_memory; mem <= max_memory; mem *= mem_multiply) {
+					b->Iterations(mem * 250 * 100 / area + 5000)->
+							Args({fib_arg.first, fib_arg.second,
+								  std::min(mem, max_memory), area, slide_speed});
+				}
+}
+
+void longSequence(benchmark::internal::Benchmark* b) {
+	int min_memory = 1;
+	int max_memory = maxKB;
+	constexpr int mem_multiply = 4;
+
+	std::vector<int> areas = {60, 95};
+	std::vector<std::pair<int, int>> fib_args = {{28, 35}};
+	std::vector<int> slide = {0, 10};
+
+	b->ArgNames({"fib_min", "fib_max", "memory", "area", "mean_varying_rate"});
+
+	for (auto& slide_speed : slide)
+		for (auto& fib_arg : fib_args)
+			for (auto& area : areas)
 				for (int mem = min_memory;; mem *= mem_multiply) {
-					b->Args({fib_arg.first, fib_arg.second,
-							 std::min(mem, max_memory), area, slide_speed});
+					b->Iterations(mem * 1024 * 10 * 100 / area)->Args({fib_arg.first, fib_arg.second,
+																	   std::min(mem, max_memory), area, slide_speed});
 					if (mem >= max_memory)
 						break;
 				}
 }
 
-#define BENCH_ARGS Apply(SeqArguments)
+BENCHMARK_TEMPLATE(BM, DummyContainer)->Iterations(2000)->Args({28, 35, 100, 50, 0});
 
-BENCHMARK_TEMPLATE(BM, DummyContainer)->BENCH_ARGS;
 
-#if SHORT_BENCHMARK
-BENCHMARK_TEMPLATE(BM, WeightedSearchTree<0>)->BENCH_ARGS;
-BENCHMARK_TEMPLATE(BM, WeightedSearchTree<1>)->BENCH_ARGS;
-BENCHMARK_TEMPLATE(BM, WeightedSearchTree<2>)->BENCH_ARGS;
-BENCHMARK_TEMPLATE(BM, WeightedSearchTree<4>)->BENCH_ARGS;
-BENCHMARK_TEMPLATE(BM, WeightedSearchTree<16>)->BENCH_ARGS;
-#else
-BENCHMARK_TEMPLATE(BM, WeightedSearchTree<1>)->BENCH_ARGS; //TODO Set optimal rate
-#endif
+BENCHMARK_TEMPLATE(BM, WeightedSearchTree<0>)->Apply(shortSequence);
+BENCHMARK_TEMPLATE(BM, WeightedSearchTree<1>)->Apply(shortSequence);
+BENCHMARK_TEMPLATE(BM, WeightedSearchTree<2>)->Apply(shortSequence);
+BENCHMARK_TEMPLATE(BM, WeightedSearchTree<4>)->Apply(shortSequence);
+BENCHMARK_TEMPLATE(BM, WeightedSearchTree<16>)->Apply(shortSequence);
 
-#if SHORT_BENCHMARK
-BENCHMARK_TEMPLATE(BM, PriorityHashtable<0>)->BENCH_ARGS;
-BENCHMARK_TEMPLATE(BM, PriorityHashtable<1>)->BENCH_ARGS;
-BENCHMARK_TEMPLATE(BM, PriorityHashtable<4>)->BENCH_ARGS;
-BENCHMARK_TEMPLATE(BM, PriorityHashtable<16>)->BENCH_ARGS;
-BENCHMARK_TEMPLATE(BM, LruHashtable<>)->BENCH_ARGS;
-BENCHMARK_TEMPLATE(BM, LfuHashtable<>)->BENCH_ARGS;
-#else
-BENCHMARK_TEMPLATE(BM, PriorityHashtable<1>)->BENCH_ARGS; //TODO Set optimal rate
-#endif
 
-#if SHORT_BENCHMARK
-BENCHMARK_TEMPLATE(BM, BottomNodeSplayTree<CanonicalSplayStrategy>)->BENCH_ARGS;
-BENCHMARK_TEMPLATE(BM, BottomNodeSplayTree<AccessCountSplayStrategy>)->BENCH_ARGS;
-BENCHMARK_TEMPLATE(BM, BottomNodeSplayTree<WstSplayStrategy<1>>)->BENCH_ARGS;
+BENCHMARK_TEMPLATE(BM, PriorityHashtable<0>)->Apply(shortSequence);
+BENCHMARK_TEMPLATE(BM, PriorityHashtable<1>)->Apply(shortSequence);
+BENCHMARK_TEMPLATE(BM, PriorityHashtable<4>)->Apply(shortSequence);
+BENCHMARK_TEMPLATE(BM, PriorityHashtable<16>)->Apply(shortSequence);
+BENCHMARK_TEMPLATE(BM, LruHashtable<>)->Apply(shortSequence);
+BENCHMARK_TEMPLATE(BM, LfuHashtable<>)->Apply(shortSequence);
 
-BENCHMARK_TEMPLATE(BM, LruSplayTree<CanonicalSplayStrategy>)->BENCH_ARGS;
-BENCHMARK_TEMPLATE(BM, LruSplayTree<AccessCountSplayStrategy>)->BENCH_ARGS;
-BENCHMARK_TEMPLATE(BM, LruSplayTree<WstSplayStrategy<1>>)->BENCH_ARGS;
-BENCHMARK_TEMPLATE(BM, LruSplayTree<ParametrizedAccessCountSplayStrategy<2, 1, 8>>)->BENCH_ARGS;
-BENCHMARK_TEMPLATE(BM, LruSplayTree<ParametrizedAccessCountSplayStrategy<16, 1, 255>>)->BENCH_ARGS;
+BENCHMARK_TEMPLATE(BM, BottomNodeSplayTree<CanonicalSplayStrategy>)->Apply(shortSequence);
+BENCHMARK_TEMPLATE(BM, BottomNodeSplayTree<AccessCountSplayStrategy>)->Apply(shortSequence);
+BENCHMARK_TEMPLATE(BM, BottomNodeSplayTree<WstSplayStrategy<1>>)->Apply(shortSequence);
 
-BENCHMARK_TEMPLATE(BM, LfuSplayTree<CanonicalSplayStrategy>)->BENCH_ARGS;
-BENCHMARK_TEMPLATE(BM, LfuSplayTree<AccessCountSplayStrategy>)->BENCH_ARGS;
-BENCHMARK_TEMPLATE(BM, LfuSplayTree<WstSplayStrategy<1>>)->BENCH_ARGS;
-BENCHMARK_TEMPLATE(BM, LfuSplayTree<ParametrizedAccessCountSplayStrategy<2, 1, 8>>)->BENCH_ARGS;
-BENCHMARK_TEMPLATE(BM, LfuSplayTree<ParametrizedAccessCountSplayStrategy<16, 1, 255>>)->BENCH_ARGS;
-#else
-BENCHMARK_TEMPLATE(BM, BottomNodeSplayTree<CanonicalSplayStrategy>)->BENCH_ARGS;
-#endif
+BENCHMARK_TEMPLATE(BM, LruSplayTree<CanonicalSplayStrategy>)->Apply(shortSequence);
+BENCHMARK_TEMPLATE(BM, LruSplayTree<AccessCountSplayStrategy>)->Apply(shortSequence);
+BENCHMARK_TEMPLATE(BM, LruSplayTree<WstSplayStrategy<1>>)->Apply(shortSequence);
+BENCHMARK_TEMPLATE(BM, LruSplayTree<ParametrizedAccessCountSplayStrategy<2, 1, 8>>)->Apply(shortSequence);
+BENCHMARK_TEMPLATE(BM, LruSplayTree<ParametrizedAccessCountSplayStrategy<16, 1, 255>>)->Apply(shortSequence);
 
+BENCHMARK_TEMPLATE(BM, LfuSplayTree<CanonicalSplayStrategy>)->Apply(shortSequence);
+BENCHMARK_TEMPLATE(BM, LfuSplayTree<AccessCountSplayStrategy>)->Apply(shortSequence);
+BENCHMARK_TEMPLATE(BM, LfuSplayTree<WstSplayStrategy<1>>)->Apply(shortSequence);
+BENCHMARK_TEMPLATE(BM, LfuSplayTree<ParametrizedAccessCountSplayStrategy<2, 1, 8>>)->Apply(shortSequence);
+BENCHMARK_TEMPLATE(BM, LfuSplayTree<ParametrizedAccessCountSplayStrategy<16, 1, 255>>)->Apply(shortSequence);
+
+BENCHMARK_TEMPLATE(ParallelBM, ConcurrentPriorityHashtable<false>)
+->ThreadRange(1, maxThreads)->Apply(shortSequence);
+BENCHMARK_TEMPLATE(ParallelBM, ConcurrentPriorityHashtable<true>)
+->ThreadRange(1, maxThreads)->Apply(shortSequence);
+
+BENCHMARK_TEMPLATE(ParallelBM, CoarseLockAdapter<WeightedSearchTree<0>>)
+->ThreadRange(1, maxThreads)->Apply(shortSequence);
+BENCHMARK_TEMPLATE(ParallelBM, CoarseLockAdapter<BottomNodeSplayTree<CanonicalSplayStrategy>>)
+->ThreadRange(1, maxThreads)->Apply(shortSequence);
+
+BENCHMARK_TEMPLATE(ParallelBM, BinningConcurrentAdapter<WeightedSearchTree<0>, 8>)
+->ThreadRange(1, maxThreads)->Apply(shortSequence);
+BENCHMARK_TEMPLATE(ParallelBM, BinningConcurrentAdapter<BottomNodeSplayTree<CanonicalSplayStrategy>, 8>)
+->ThreadRange(1, maxThreads)->Apply(shortSequence);
 
 BENCHMARK_MAIN();
